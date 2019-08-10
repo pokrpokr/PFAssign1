5f74c8367839262c9e20c5eaf822181348bff49d
chai <yeshengdemogujun@gmail.com>
2019-08-10 18:54:12 +1000

    version-1

--- src/RobotControl.java
+++ src/RobotControl.java
@@ -1,4 +1,7 @@
- class RobotControl
+import java.io.InputStream;
+import java.util.*;
+
+class RobotControl
  {
    private Robot r;
    
@@ -6,61 +9,70 @@
    {
        this.r = r;
    }
-
+   
+//   public static void main(String[] args) {
+//	   System.out.println("*********");
+//	   int[] barHeights = new int[6];
+//	   int[] blockHeights = new int[4];
+//	   int[] required = new int[4];
+//	   boolean ordered = false;
+//	   Scanner scan = new Scanner(System.in);
+//	   
+//	   for(int i = 0; i < 6; i++) {
+//		   System.out.println("Please input " + (i+1) + " bar height");
+//		   barHeights[i] = scan.nextInt();
+//	   }
+//	   
+//	   for(int i = 0; i < 4; i++) {
+//		   System.out.println("Please input " + (i+1) + " block height");
+//		   blockHeights[i] = scan.nextInt();
+//	   }
+//	   control(barHeights, blockHeights, required, ordered);
+//   }
+   
    public void control(int barHeights[], int blockHeights[], int required[], boolean ordered)
-   {
-	   // The first past can be solved easily with out any arrays as the height of bars and blocks are fixed.
-	   // Use the method r.up(), r.down(), r.extend(), r.contract(), r.raise(), r.lower(), r.pick(), r.drop()
-	   // The code below will cause first arm to be moved up, the second arm to the right and the third to be lowered. 
-	  // move the first arm up by one unit
-	   // move the second arm to the right by one unit
-	   r.lower();
-	   // lowering the third arm by one unit
-	   // Part B requires you to access the array barHeights passed as argument as the robot arm must move
-	   // over the bars
-	   // The third part requires you to access the arrays barHeights and blockHeights 
-	   // as the heights of bars and blocks are allowed to vary through command line arguments
-	   // The fourth part allows the user  to specify the order in which bars must 
-	   // be placed in the target column. This will require you to use the use additional column
-	   // which can hold temporary values
-	   // The last part requires you to write the code to move from source column to target column using
-	   // an additional temporary column but without placing a larger block on top of a smaller block 
-	   int times = 0;
-	   int block_count = 4;
-	   int block_h = 2;
-	   int up_h = block_h * block_count + 3;
-	   int down_h = 1;
-	   int block_drop = 0;
-	   int temp;
-	   int length = 9;
-	   for (int i = 0; i < 4; i++ ) {
-		   times+=2;
-		   if (times > 4) {
-			   for (int j = 0; j < (temp = up_h - 4 - down_h); j++)
-				   r.up();
-			   System.out.println("@@@@@"+temp);
-			   Mv_extand(r, length);
-			   r.pick();
-			   Mv_contract(r, length);
-		   }else {
-			   System.out.println("@@-@@"+times);
-			   for (int j = 0; j < (temp = up_h - times - down_h); j++)
-				   r.up();
-			   System.out.println("*****"+temp);
-			   Mv_extand(r, length);
-			   r.pick();
-			   Mv_contract(r, length);
-			   while(0 < (temp-block_h*block_drop-block_h)) {
-				   System.out.println("&&&&&"+temp);
-				   r.down();
-				   temp--;
-			   }
-		   }
-		   r.drop();
-		   down_h+=2;
-		   block_drop++;
-	   }
+   {   int h; // 2 < h < 14
+       int w; // 1 < w < 10
+       int d; // 0 < d < h
+       int block_h;
+	   int max_barH = Math.max(MyMath.max(barHeights[0], barHeights[1], barHeights[2]),
+			   MyMath.max(barHeights[3], barHeights[4], barHeights[5]));
+	   int max_blockH = maxBlockH(blockHeights);
+	   int sum_blockH = blockHSum(blockHeights);
+	   int up_h = sum_blockH >= (max_blockH + max_barH)? sum_blockH:max_barH + max_blockH;
+	   
+	   Mv_up(r, up_h);
+	   Mv_extend(r, 9);
+	   Mv_lower(r, 1);
+	   r.pick();
+	   Mv_contract(r,9);
+//	   Mv_lower(r,up_h - block_h);
+	   
    }
+   
+   private int maxBlockH(int[] blockHeights) {
+	   int max_blockH = blockHeights[0];
+	   for(int i = 1; i < blockHeights.length; i++) {
+		   max_blockH = Math.max(max_blockH, blockHeights[i]);
+	   }
+	   return max_blockH;
+   }
+   
+   private int blockHSum(int[] blockHeights) {
+	   int sum = 0;
+	   for(int i = 0; i < blockHeights.length; i++)
+		   sum += blockHeights[i];
+	   return sum;
+   }
+   
+   private int find_index(int[] blockHeights, int value) {
+	   int index = 0;
+	   for(int i = 0; i < blockHeights.length; i++)
+		   if(blockHeights[i] == value)
+			   index = i;
+	   return index;	   
+   }
+
    private void Mv_up(Robot r, int sourceHt) {
 	   for(int i = 0; i < sourceHt; i++)
 		   r.up();
@@ -71,14 +83,24 @@
 		   r.down();
    }
    
-   private void Mv_extand(Robot r, int length ) {
-	   for (int i = 0; i < length; i++)
+   private void Mv_extend(Robot r, int ex_length) {
+	   for (int i = 0; i < ex_length; i++)
 		   r.extend(); 
    }
    
-   private void Mv_contract(Robot r, int length) {
-	   for (int i = 0; i < length; i++)
+   private void Mv_contract(Robot r, int con_length) {
+	   for (int i = 0; i < con_length; i++)
 		   r.contract();
+   }
+   
+   private void Mv_raise(Robot r, int ra_height) {
+	   for(int i = 0; i < ra_height; i++)
+		   r.raise();
+   }
+   
+   private void Mv_lower(Robot r, int lw_height) {
+	   for(int i = 0; i < lw_height; i++)
+		   r.lower();
    }
 }  
 
